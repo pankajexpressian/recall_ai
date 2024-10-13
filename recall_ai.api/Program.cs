5e71031e1605ae973cae74c4ad4332c38677a76d
ArunVash <arun.vashishtha@nagarro.com>
2024-10-13 23:23:44 +0530

    remove changes

--- recall_ai.api/Program.cs
+++ recall_ai.api/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<TextEmbeddingService>();
 builder.Services.AddSingleton<PineconeClient>();
-builder.Services.AddSingleton<OpenAiService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
