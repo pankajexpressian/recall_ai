fc9633b3ebad6b0a77e71bbb444015a84bba2db3
Swati Bhalla <swatib@nedbank.co.za>
2024-10-13 22:05:46 +0530

    view notes ui

--- recall_ai_ui/src/app/app.constants.ts
+++ recall_ai_ui/src/app/app.constants.ts
@@ -2,43 +2,43 @@ export const MOODS = [
     {
       name: 'ANGER',
       value: 0,
-      color: '#ffaaae',
+      color: 'orange',
       emoji: '😡'  // Angry face emoji
     },
     {
       name: 'DISGUST',
       value: 1,
-      color: '#8ef1b3',
+      color: 'green',
       emoji: '🤢'  // Nauseated face emoji
     },
     {
       name: 'FEAR',
       value: 2,
-      color: '#fee351',
+      color: 'yellow',
       emoji: '😨'  // Fearful face emoji
     },
     {
       name: 'JOY',
       value: 3,
-      color: '#9cc9ff',
+      color: 'blue',
       emoji: '😊'  // Smiling face with smiling eyes emoji
     },
     {
       name: 'NEUTRAL',
       value: 4,
-      color: '#d5d9df',
+      color: 'grey',
       emoji: '😐'  // Neutral face emoji
     },
     {
       name: 'SADNESS',
       value: 5,
-      color: '#acb8fe',
+      color: 'purple',
       emoji: '😢'  // Crying face emoji
     },
     {
       name: 'SURPRISE',
       value: 6,
-      color: '#fbaed9',
+      color: 'brown',
       emoji: '😲'  // Astonished face emoji
     }
   ];
